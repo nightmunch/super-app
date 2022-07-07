3bebf37e12d806ca256a2b6a07be853f27ed2a01
Shahrin Amin <shahrinamin.sharifudin@tm.com.my>
2022-07-07 21:08:56 +0800

    trpc initial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,12 @@ import "../styles/globals.css";
 import { MainLayout } from "../components/MainLayout";
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { withTRPC } from "@trpc/next";
+import { loggerLink } from "@trpc/client/links/loggerLink";
+import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
+import superjson from "superjson";
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Head>
@@ -15,3 +19,41 @@ export default function App({ Component, pageProps }: AppProps) {
 		</>
 	);
 }
+
+export default withTRPC({
+	config({ ctx }) {
+		const url = process.env.NEXT_PUBLIC_VERCEL_URL
+			? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+			: "http://localhost:3000/api/trpc";
+
+		const links = [
+			loggerLink(),
+			httpBatchLink({
+				maxBatchSize: 10,
+				url,
+			}),
+		];
+
+		return {
+			queryClientConfig: {
+				defaultOptions: {
+					queries: {
+						staleTime: 60,
+					},
+				},
+			},
+			headers() {
+				if (ctx?.req) {
+					return {
+						...ctx.req.headers,
+						"x-ssr": "1",
+					};
+				}
+				return {};
+			},
+			links,
+			transformer: superjson,
+		};
+	},
+	ssr: true,
+})(App);
