3ce18162cd4cb3fefa0e8dd2a62f43d1d2a05a33
Shahrin Amin <shahrinamin.sharifudin@tm.com.my>
2022-08-26 19:07:45 +0800

    dynamic doughnut sizing

--- components/Charts.tsx
+++ components/Charts.tsx
@@ -1,4 +1,5 @@
 import "chart.js/auto";
+import { useEffect, useState } from "react";
 import { Chart } from "react-chartjs-2";
 
 type Input = {
@@ -10,11 +11,44 @@ type Input = {
 	}[];
 };
 
+type WindowSize = {
+	width: number | undefined;
+	height: number | undefined;
+};
+
 export const Doughnut: React.FunctionComponent<Input> = ({ title, data }) => {
+	const [windowSize, setWindowSize] = useState<WindowSize>({
+		width: undefined,
+		height: undefined,
+	});
+
+	// Handler to call on window resize
+	function handleResize() {
+		// Set window width/height to state
+		setWindowSize({
+			width: window.innerWidth,
+			height: window.innerHeight,
+		});
+	}
+
+	useEffect(() => {
+		// only execute all the code below in client side
+		if (typeof window !== "undefined") {
+			// Add event listener
+			window.addEventListener("resize", handleResize);
+
+			// Call handler right away so state gets updated with initial window size
+			handleResize();
+
+			// Remove event listener on cleanup
+			return () => window.removeEventListener("resize", handleResize);
+		}
+	}, []);
+
 	return (
 		<>
 			<h1 className="text-center">{title}</h1>
-			<div className="sm:w-2/5 mx-auto">
+			<div className="w-72 sm:w-96 mx-auto">
 				<Chart
 					type="doughnut"
 					data={{
@@ -40,7 +74,7 @@ export const Doughnut: React.FunctionComponent<Input> = ({ title, data }) => {
 							},
 						},
 						responsive: true,
-						cutout: 80,
+						cutout: windowSize.width! > 500 ? 120 : 70,
 					}}
 				/>
 			</div>
