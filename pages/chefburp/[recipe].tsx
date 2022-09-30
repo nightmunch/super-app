8591534b7d4c4e94d8b9407520639ab0d2982927
Shahrin Amin <shahrinamin.sharifudin@tm.com.my>
2022-09-30 16:55:17 +0800

    loading recipe

--- pages/chefburp/[recipe].tsx
+++ pages/chefburp/[recipe].tsx
@@ -53,84 +53,110 @@ export default function Recipe() {
 
 	return (
 		<>
-			<div className="card bg-neutral text-neutral-content">
-				<div className="card-body">
-					{editable ? (
-						<input
-							type="text"
-							className="input"
-							value={title == "" ? (recipe.data?.title as string) : title}
-							onChange={(e) => setTitle(e.target.value)}
-						/>
-					) : (
-						<h1 className="text-xl font-semibold text-primary capitalize">
-							{recipe.data?.title}
-						</h1>
-					)}
-					{editable ? (
-						<input
-							type="text"
-							className="input"
-							value={
-								description == ""
-									? (recipe.data?.description as string)
-									: description
-							}
-							onChange={(e) => setDescription(e.target.value)}
-						/>
-					) : (
-						<p className="text-sm">{recipe.data?.description}</p>
-					)}
-					<div className="divider my-0"></div>
-					{editable ? <MenuBar editor={ingredients} /> : <></>}
-					<h2 className="text-lg font-semibold text-primary">Ingredients</h2>
-					<EditorContent editor={ingredients} />
-					{editable ? <MenuBar editor={steps} /> : <></>}
-					<h2 className="text-lg font-semibold text-primary text-">Steps</h2>
-					<EditorContent editor={steps} />
-					<div className="divider my-0"></div>
-					<button
-						className="btn btn-sm btn-info"
-						onClick={async () => {
-							if (editable) {
-								await updateRecipe.mutateAsync({
-									id,
-									title: title == "" ? (recipe.data?.title as string) : title,
-									description:
-										description == ""
-											? (recipe.data?.description as string)
-											: description,
-									ingredient: ingredients?.getHTML() as string,
-									step: steps?.getHTML() as string,
-								});
-							}
-
-							ingredients?.setEditable(!editable);
-							steps?.setEditable(!editable);
-							setEditable(!editable);
-						}}
+			{!ingredients?.getText() ? (
+				<div className="inline-flex items-center">
+					<svg
+						className="animate-spin mr-3 h-5 w-5 text-white"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
 					>
-						{editable ? "Done" : "Edit"}
-					</button>
-					<Link href={"/chefburp"}>
-						<button className="btn btn-sm btn-warning mt-2">Back</button>
-					</Link>
-					{sessionData?.user?.role == "Admin" ? (
-						<div className="flex">
-							<div className="tooltip" data-tip="Remove Recipe">
-								<label
-									htmlFor="delete-recipe"
-									className="btn btn-square btn-error mt-2"
-								>
-									<FaTrash />
-								</label>
-							</div>
-						</div>
-					) : (
-						<></>
-					)}
+						<circle
+							className="opacity-25"
+							cx="12"
+							cy="12"
+							r="10"
+							stroke="currentColor"
+							strokeWidth="4"
+						></circle>
+						<path
+							className="opacity-75"
+							fill="currentColor"
+							d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+						></path>
+					</svg>
+					Loading...
 				</div>
-			</div>
+			) : (
+				<div className="card bg-neutral text-neutral-content">
+					<div className="card-body">
+						{editable ? (
+							<input
+								type="text"
+								className="input"
+								value={title == "" ? (recipe.data?.title as string) : title}
+								onChange={(e) => setTitle(e.target.value)}
+							/>
+						) : (
+							<h1 className="text-xl font-semibold text-primary capitalize">
+								{recipe.data?.title}
+							</h1>
+						)}
+						{editable ? (
+							<input
+								type="text"
+								className="input"
+								value={
+									description == ""
+										? (recipe.data?.description as string)
+										: description
+								}
+								onChange={(e) => setDescription(e.target.value)}
+							/>
+						) : (
+							<p className="text-sm">{recipe.data?.description}</p>
+						)}
+						<div className="divider my-0"></div>
+						{editable ? <MenuBar editor={ingredients} /> : <></>}
+						<h2 className="text-lg font-semibold text-primary">Ingredients</h2>
+						<EditorContent editor={ingredients} />
+						{editable ? <MenuBar editor={steps} /> : <></>}
+						<h2 className="text-lg font-semibold text-primary text-">Steps</h2>
+						<EditorContent editor={steps} />
+						<div className="divider my-0"></div>
+						<button
+							className="btn btn-sm btn-info"
+							onClick={async () => {
+								if (editable) {
+									await updateRecipe.mutateAsync({
+										id,
+										title: title == "" ? (recipe.data?.title as string) : title,
+										description:
+											description == ""
+												? (recipe.data?.description as string)
+												: description,
+										ingredient: ingredients?.getHTML() as string,
+										step: steps?.getHTML() as string,
+									});
+								}
+
+								ingredients?.setEditable(!editable);
+								steps?.setEditable(!editable);
+								setEditable(!editable);
+							}}
+						>
+							{editable ? "Done" : "Edit"}
+						</button>
+						<Link href={"/chefburp"}>
+							<button className="btn btn-sm btn-warning mt-2">Back</button>
+						</Link>
+						{sessionData?.user?.role == "Admin" ? (
+							<div className="flex">
+								<div className="tooltip" data-tip="Remove Recipe">
+									<label
+										htmlFor="delete-recipe"
+										className="btn btn-square btn-error mt-2"
+									>
+										<FaTrash />
+									</label>
+								</div>
+							</div>
+						) : (
+							<></>
+						)}
+					</div>
+				</div>
+			)}
 			<ModalRemove
 				htmlfor="delete-recipe"
 				title="Delete this recipe?"
@@ -140,7 +166,6 @@ export default function Recipe() {
 				alertMessage={"Recipe is successfully deleted!"}
 				alertDispatch={alertDispatch}
 			/>
-
 			<Alert state={alertState} dispatch={alertDispatch} />
 		</>
 	);
